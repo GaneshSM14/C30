3495e85ebd9262d566444ce6a4ea8e57b606d4bb
GaneshSM14 <65331734+GaneshSM14@users.noreply.github.com>
2021-01-26 14:42:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var box1,box2,box3,box4,box5,box6,box7,box8,box9
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+const Constraint=Matter.Constraint;
+var engine,world;
+function setup() {
+  createCanvas(800,400);
+  engine=Engine.create();
+  world=engine.world;
+ground1=createSprite(400,380,800,20);
+ box1=new box(330.235,30,40);
+  box2=new box(360,235,30,40);
+  box3=new box(390,235,30,40);
+  box4=new box(420,235,30,30);
+  box5=new box(450,235,30,40);
+  box6=new box (360,195,30,40);
+  box7=new box(390,195,30,40);
+  box8=new box(420,195,30,40);
+  box9=new box(390,155,30,40);
+  pologon=createSprite(100,100,50,50);
+  slingshot1=new SlingShot(pologon.body,{x:100,y:100});
+}
+
+function draw() {
+  background(255,255,255); 
+  Engine.update(engine); 
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display();
+  box9.display();
+  slingshot1.display();
+  drawSprites();
+}
